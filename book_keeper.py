b467a634cb462e132685126dc2f623b33b3d0b8f
sonchi <banabulasma@gmail.com>
2017-12-16 13:29:59 -0800

    Implemented multi-threaded book keeper logic. Moved globals to a separate file. Updated .gitignore to include txt files.

--- book_keeper.py
+++ book_keeper.py
@@ -1,2 +1,77 @@
 import gdax
+import threading
+import time
+import random
+from globals import BOOKS, BOOK_CAPACITY
+from globals import BOOK_INDEX
+from globals import PRODUCTS
+from globals import LOCKS
 
+
+class BookKeeper(threading.Thread):
+  def __init__(self, product, books, book_index, locks):
+    threading.Thread.__init__(self)
+    self.daemon = True
+    self.product = product
+    assert type(locks) == dict
+    self.lock = locks[self.product]
+    assert type(books) == dict
+    self.book = books[self.product]
+    assert type(self.book) == list
+    assert type(book_index) == dict
+    self.book_index = book_index[self.product]
+    assert type(self.book_index) == list
+    assert len(self.book_index) == 1
+    self.public_client = gdax.PublicClient()
+
+  def run(self):
+    exceed_count = 0
+    while True:
+      book = self.public_client.get_product_order_book(self.product, level=2)
+      if 'message' in book:
+        exceed_count = exceed_count + 1
+        print('{} XXXXXX {}'.format(self.product, exceed_count))
+        time.sleep(0.5 * exceed_count)
+        continue
+      else:
+        exceed_count = 0
+      # Increment the index, this is the index of the book that we are updating.
+      index_ = (self.book_index[0] + 1) % BOOK_CAPACITY
+      #print('Updating {} for {}'.format(index_, self.product))
+      # Update the book at index_
+      self.book[index_]['bids'] = book['bids'].copy()
+      self.book[index_]['asks'] = book['asks'].copy()
+      # Update the shared book index.
+      with self.lock:
+        self.book_index[0] = index_
+
+class BookReader(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.daemon = True
+    # Warm up for book keepers
+    time.sleep(5)
+
+  def run(self):
+    while True:
+      print('\n\n')
+      for p in PRODUCTS:
+        index_ = -1
+        with LOCKS[p]:
+          index_ = BOOK_INDEX[p][0]
+        print('{} index {}'.format(p, index_))
+      print('\n\n')
+      time.sleep(1)
+
+
+if __name__ == '__main__':
+  thread_list = list()
+  for p in PRODUCTS:
+    t = BookKeeper(p, BOOKS, BOOK_INDEX, LOCKS)
+    thread_list.append(t)
+  for t in thread_list:
+    t.start()
+  r = BookReader()
+  r.start()
+  while True:
+    time.sleep(1)
---
+++ globals.py
@@ -0,0 +1,88 @@
+import threading
+
+# Initialize the books for all products. We keep one book per product.
+# Each book contains @capacity snapshots updated in a round-robin
+# fashion by the BookKeeper of the corresponding product.
+# Each snapshot contains a 'bids' dictionary and an 'asks' dictionary
+# initialized to empty lists.
+#
+# BOOKS = { 'BTC-USD' : [{bids : [[]], asks : [[]]},
+#                        {bids : [[]], asks : [[]]},
+#
+#                         ...
+#
+#                        {bids : [[]], asks : [[]]}],
+#
+#           'LTC-USD' : [{bids : [[]], asks : [[]]},
+#                        {bids : [[]], asks : [[]]},
+#
+#                         ...
+#
+#                        {bids : [[]], asks : [[]]}],
+#
+#           'ETH-USD' : [{bids : [[]], asks : [[]]},
+#                        {bids : [[]], asks : [[]]},
+#
+#                         ...
+#
+#                        {bids : [[]], asks : [[]]}],
+#
+#           'ETH-BTC' : [{bids : [[]], asks : [[]]},
+#                        {bids : [[]], asks : [[]]},
+#
+#                         ...
+#
+#                        {bids : [[]], asks : [[]]}],
+#
+#           'LTC-BTC' : [{bids : [[]], asks : [[]]},
+#                        {bids : [[]], asks : [[]]},
+#
+#                         ...
+#
+#                        {bids : [[]], asks : [[]]}] }
+def init_books(products, capacity):
+  books = dict()
+  for p in products:
+    books[p] = list()
+    for i in range(capacity):
+      books[p].append({'bids': [[]], 'asks': [[]]})
+    assert len(books[p]) == capacity
+  assert len(books) == len(products)
+  return books
+
+# BOOK_INDEX maps a product to the index of the most up-to-date snapshot in the list.
+# The value is stored in a list to pass it by reference to BookKeeper instances.
+#
+# Suppose
+#   BOOK_INDEX['BTC-USD'] == [5] , then
+#   BOOKS['BTC-USD'][5]
+# contains the most up-to-date snapshot for 'BTC-USD'.
+#
+# BOOK_INDEX = { 'BTC-USD' : [-1],
+#                'LTC-USD' : [-1],
+#                'ETH-USD' : [-1],
+#                'LTC-BTC' : [-1],
+#                'ETH-BTC' : [-1] }
+def init_book_index(products):
+  book_index = dict()
+  for p in products:
+    book_index[p] = [-1]
+  assert len(book_index) == len(products)
+  return book_index
+
+# Initialize locks. We keep one lock per product for synchronizing access to book_index.
+def init_locks(products):
+  locks = dict()
+  for p in products:
+    locks[p] = threading.Lock()
+  return locks
+
+BOOK_CAPACITY = 30
+
+PRODUCTS = ['BTC-USD', 'LTC-USD', 'ETH-USD', 'ETH-BTC', 'LTC-BTC']
+
+BOOKS = init_books(PRODUCTS, BOOK_CAPACITY)
+
+BOOK_INDEX = init_book_index(PRODUCTS)
+
+LOCKS = init_locks(PRODUCTS)
